a698ca98d630c6f05a1438f2f46f6db8c4d11f0c
Milad Ibra <m.ebrahimi@snapp.cab>
2020-08-22 22:06:07 +0430

    Fix ServeHTTP method in service pkg

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -17,12 +17,24 @@ func (s *Service) StartHealthCheck() {
 }
 
 func (s *Service) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	uri := s.LB.Balance(s.Backends)
+	uri := s.LB.Balance(s.getHealthyBackends())
 	remote, err := url.Parse(uri.Addr)
 	if err != nil {
-		panic(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`500 - Internal Server Error`))
+		return
 	}
 
 	proxy := httputil.NewSingleHostReverseProxy(remote)
 	proxy.ServeHTTP(w, r)
+}
+
+func (s *Service) getHealthyBackends() []Backend {
+	result := make([]Backend, 0)
+	for _, backend := range s.Backends {
+		if backend.Status == Available {
+			result = append(result, backend)
+		}
+	}
+	return result
 }
